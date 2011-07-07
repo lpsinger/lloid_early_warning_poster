8346a08e31d038f7b7a822275f7d2186b1602c4e
Leo Singer <leo.singer@ligo.org>
2011-07-07 13:46:39 -0700

    Fixed image that had been cut off

--- envelope.py
+++ envelope.py
@@ -38,6 +38,6 @@ pylab.xticks([-x.end for x in bank.bank_fragments if x.end > 10], [str(x.end) fo
 pylab.grid()
 pylab.xlabel('time relative to coalescence (s)')
 pylab.ylabel(r'strain amplitude')
-pylab.subplots_adjust(left=0.075, right=0.97, top=0.95, bottom=0.15)
+pylab.subplots_adjust(left=0.075, right=0.97, top=0.95, bottom=0.175)
 pylab.gca().set_axis_bgcolor('#E6E6E6')
 pylab.savefig(sys.argv[1])
