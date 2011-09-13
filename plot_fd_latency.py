9ecfc0eaeb0595f7390625ef878cfba35ba88e0e
Leo Singer <leo.singer@ligo.org>
2011-09-13 12:05:24 -0700

    Stuff I forgot to commit

--- plot_fd_latency.py
+++ plot_fd_latency.py
@@ -2,10 +2,10 @@ import pylab
 import sys
 
 pylab.figure(figsize=(5, 4))
-a = pylab.linspace(0., 1., 1000.)
-pylab.plot(a, 2 * (1 + a) / a, 'k', lw=2)
+a = pylab.linspace(0.001, 1., 1000.)
+pylab.plot(a, 1 + 1 / a, 'k', lw=2)
 pylab.ylim(0, 40)
 pylab.xlabel('(latency) / (template length)')
-pylab.ylabel(r'operations / sample / $\lg (\textrm{template length})$')
+pylab.ylabel(r'operations / sample / $2 \lg (\textrm{template length})$')
 pylab.gca().set_axis_bgcolor('#E6E6E6')
 pylab.savefig(sys.argv[1])
